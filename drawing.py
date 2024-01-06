10f597ddf8db35c6f1af624c9626d83a7d5b1232
chanithacri <chanithagunasekera@gmail.com>
2024-01-06 13:05:33 +0530

    Refactor drawing.py to include a function for generating random colors and to draw a colorful circle pattern.

--- drawing.py
+++ drawing.py
@@ -1,5 +1,25 @@
 import turtle
-turtle.bgcolor("white")
-t = turtle.pen()
-t.pencolor("blue")
-t.forward(100)
+import random
+a = int(input())
+
+
+def random_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    return (r, g, b)
+
+
+window = turtle.Screen()
+window.bgcolor("black")
+window.colormode(255)
+draw = turtle.Turtle()
+draw.speed(0)  # Setting the speed to 0, which is the fastest
+
+# Draw colorful circle pattern
+for i in range(a):
+    draw.color(random_color())
+    draw.circle(100)
+    draw.right(a * 0.1)
+
+turtle.done()
