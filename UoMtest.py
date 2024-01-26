24e2b1aef4f115d3701e5a40fe84bab01733b9ac
chanithacri <chanithagunasekera@gmail.com>
2024-01-26 19:49:05 +0530

    Remove old file reading and writing logic, and add a new loop to print characters.

--- UoMtest.py
+++ UoMtest.py
@@ -1,11 +1,2 @@
-a = []
-with open("file1.txt", "r") as f:
-    for i in range(2):
-        a.append(f.readline())
-    with open("file2.txt", "w") as n:
-        for i in a:
-            n.write(i)
-
-with open("file2.txt", "r") as d:
-    z = d.read()
-    print(z)
+for i in range(3,2,-1):
+    print('!'*i)
