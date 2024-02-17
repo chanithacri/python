24b0cc3fbec6c17e3b426c2e14714ab7b9ce38dc
chanithacri <chanithagunasekera@gmail.com>
2024-02-17 13:26:51 +0530

    Update sever.py
    added code to print the link

--- sever.py
+++ sever.py
@@ -7,4 +7,5 @@ Handler = http.server.SimpleHTTPRequestHandler
 
 with socketserver.TCPServer(("", PORT), Handler) as httpd:
     print("Server running on port", PORT)
+    print("https://5tddfk-8000.csb.app/")
     httpd.serve_forever()
