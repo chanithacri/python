4f2f34d956b744e3270fd0de0728b71b79811178
chanithacri <chanithagunasekera@gmail.com>
2024-05-08 09:12:25 +0530

    feat: Add pandas import and load data into a DataFrame
    Import the pandas library and load the data from the "hyperskill-dataset-97901992.txt.txt" file into a DataFrame. Specify the index column as "Name". Print the first 10 rows of the DataFrame.

--- UoMtest.py
+++ UoMtest.py
@@ -1,2 +1,7 @@
-for i in range(3,2,-1):
-    print('!'*i)
+import pandas as pd
+
+# Load the text file into a DataFrame, specifying the index column as "Name"
+df = pd.read_csv("hyperskill-dataset-97901992.txt.txt", index_col="Name")
+
+# Print the first 10 rows of the DataFrame
+print(df.head(10))
