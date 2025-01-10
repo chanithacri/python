8a60fdfa99a155d5aff8704351a0f0501290c367
chanithacri <113086455+chanithacri@users.noreply.github.com>
2025-01-10 17:29:37 +0000

    c

---
+++ Fomats/Class_format.py
@@ -0,0 +1,14 @@
+# Tpical class in python 
+class ExampleClass():  # CamelCase
+    def self():
+        pass
+    def __inint__(input_1, input_n):
+        input_1 = input_1
+        input_n = input_n
+    
+    def func_1(self):
+        pass
+
+    def func_2(self):
+        a = self.input_1
+        b = self.input_n
