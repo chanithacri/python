ac62b90551a159ab5dc730e737355dc0f0192c41
chanitha <chanithagunasekera@gmail.com>
2025-01-25 22:58:39 +0530

    Update Class format

--- Fomats/Class_format.py
+++ Fomats/Class_format.py
@@ -12,7 +12,7 @@ class ExampleClass:  # CamelCase
         b = self.input_n
 
 
-# Example Class for a Cal 
+# Example Class for a Cal and parent class
 class CalClass:
     def __init__(self, a, b):
         self.value = 0
@@ -21,15 +21,33 @@ class CalClass:
     
     def add(self):
         self.value = self.num_1 + self.num_2
+        return self.value
     
     def substarct(self):
         self.value = self.num_1 - self.num_2
+        return self.value
     
     def divide(self):
         self.value = self.num_1 / self.num_2
+        return self.value
     
     def multipy(self):
         self.value = self.num_1 * self.num_2
+        return self.value
     
     # continue for all oparation
     
+
+# Child clases
+class ChildCal(CalClass):  # Child class contains all the finctios and variables of a parent class
+    pass
+
+
+a = int(input("Num 1: "))
+b = int(input("Num 2: "))
+# Parent class
+cal_1 = CalClass(a, b)
+print(cal_1.add())
+# Chiled class
+cal_2 = ChildCal(a, b)
+print(cal_2.add())
